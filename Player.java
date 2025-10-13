8533b436dd26f8ec353af91d3f37d2bcf8e98111
SoulForSeek <savoury.aderral91@posteo.com>
2025-10-13 21:02:54 +0200

    MazeGameGui almost finished

--- Player.java
+++ Player.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
 
 import javax.swing.*;
 public class Player extends JPanel implements ActionListener {
