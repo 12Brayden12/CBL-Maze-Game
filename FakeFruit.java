2ddba12eb661df96f8e9a23ac89f9d74763178c1
12Brayden12 <z.deng2@student.tue.nl>
2025-10-14 16:10:32 +0200

    FakeFruits

--- FakeFruit.java
+++ FakeFruit.java
@@ -2,5 +2,9 @@ public class FakeFruit extends GameObject{
     
     public FakeFruit() {
         super('!');
+        if (board.getValue(playerX, playerY) == '!') {
+            ControlReversal.activate();
+        }
+
     }
 }
